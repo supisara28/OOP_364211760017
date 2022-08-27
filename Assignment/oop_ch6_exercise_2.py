bb5b6ef976f35bcfe1c9696c4ad184a177191253
tooktaSupisara <supisara@rmutsvmail.com>
2022-08-27 14:33:53 +0700

    add : assignment

---
+++ Assignment/oop_ch6_exercise_2.py
@@ -0,0 +1,35 @@
+"""
+OOP Exercise Chapter 6
+
+1. จงเขียนโปรแกรมภาษาไพธอนเพื่อสร้างคลาสพาหนะชื่อ Vehicle ที่ประกอบไปด้วยคุณลักษณะ (attributes) คือ 
+ยี่ห้อรถ (brand) 
+รุ่นรถ (model)
+สีรถ (color)
+ความรเร็วสูงสุด (max speed)
+ราคา (price)
+
+2.จากข้อที่ 1 เขียนโปรแกรมเพื่อสร้างวัตถุ (object) จากคลาส Vehicle โดยรับข้อมูลจากผู้ใช้ตามคุณลักษณะ (attributes)ของคลาส
+จากนั้นแสดงข้อมูลทางหน้าจอภาพ
+
+15 นาที
+"""
+
+class Vehicle:
+    def __int__(self,brand,model,color,maxspeed,price):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.maxspeed = maxspeed
+        self.price = price
+
+    def my_vehicle(self):
+        b = input(f'Brand : ')
+        m = input(f'Model: ')
+        c = input(f'Color: ')
+        msp = input(f'Max Speed: ')
+        p = input(f'Price: ')
+        print('Brand ',b ,
+              'Model ',m,
+              'Color ',c,
+              'Max Speed ',msp,
+              'Price ',p)
