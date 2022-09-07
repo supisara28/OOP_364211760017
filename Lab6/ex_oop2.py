b3cb8a431a1554846e484385a2c411c89b91890a
tooktaSupisara <supisara.ko@rmutsvmail.com>
2022-09-07 15:28:33 +0700

    add : ex oop lab6

--- Lab6/ex_oop2.py
+++ Lab6/ex_oop2.py
@@ -7,12 +7,15 @@ Group : MIT221
 class Student:
     #class attributes
     uni = "RUTS"
+    my_std = [] #empty list
 
     def __init__(self, name, id, major):
         # object attributes
         self.name = name
         self.id = id
         self.major = major
+        # add object to my_std (list)
+        self.my_std.append(self)
 
     def introduce(self):
         print(f'My name is {self.name}, my id is {self.id} and I am studying in {self.major} majot.' )
@@ -31,3 +34,11 @@ print(s2.uni)
 Student.uni = "PSU"
 print(s1.uni)
 print(s2.uni)
+
+s1.uni = "WU"
+print(s1.uni)
+print(s2.uni)
+
+print(len(Student.my_std))
+
+print(Student.my_std[0].introduce())
